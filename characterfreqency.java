d0c73df7a05efb21b1a6d7f3ae69cacc911ded14
Thomas77719 <145203049+Thomas77719@users.noreply.github.com>
2023-10-31 13:49:49 +0530

    Add files via upload

---
+++ characterfreqency.java
@@ -0,0 +1,37 @@
+/******************************************
+ * File name   : Character frequency
+ * Discription : To  check character frequency of the  entered character in the input
+ * Author      : Thomas varghese
+ * Version     : 1.0
+ *******************************************/
+package lab;
+import java.util.Scanner ;
+public class characterfreqency {
+public static void main(String[] args){
+	  String input;
+      char checkCharacter;
+      Scanner sc = new Scanner(System.in);
+	  System.out.println("Enter the string");
+	  input=sc.nextLine();
+	  System.out.println("Enter the character which the frequency to be checked");	
+	  checkCharacter = sc.next().charAt(0);
+	  int count=frequency(input,checkCharacter);
+	  System.out.println("The number of occurance of the character in the string="+count );
+		}
+		
+		static int frequency(String input,char checkCharacter){
+			int count=0;
+			char characterArray[]=input.toCharArray();
+			for(int i=0;i<input.length();i++)
+			{
+				if(checkCharacter == characterArray[i])
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+	}
+			
+
+
