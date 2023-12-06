8dcb7fa5423fc9c03fbacabcd8d0a440bd9ac21e
Thomas <145203049+meetThomas@users.noreply.github.com>
2023-12-06 13:08:22 +0530

    Add files via upload
    inheritance 2

--- interFace.java
+++ interFace.java
@@ -1,130 +1,120 @@
-package implementance;
-import java.util.Scanner;
+package javalab;
 
- public class interFace { 
-	 public static void main(String[] args) {
-		 
-		 //Fish
-		 
-  Fish fish=new Fish();
-  System.out.println(" FISH ");
-  fish.setName("MIMI");
-  System.out.println("This fish name is "+fish.getName());
-  
-  fish.eat();
-  fish.walk();
-  Fish fish2=new Fish();
-  fish2.setName("MOMO");
-   System.out.println("This fish name is "+fish2.getName());
-   
-   //cat
-   
-  Cat cat=new Cat();
-  System.out.println("\n CAT ");
-  cat.setName("FLUFFY");
-  System.out.println("This fish name is "+cat.getName());
-  
+	import java.util.Scanner;
 
-  cat.eat();
-  cat.setName("MOOSE");
-  Cat cat2=new Cat();
-  cat2.setName("MOMO");
-   System.out.println("This fish name is "+cat2.getName());
- 
-  
-  
-  Spider spider=new Spider();
-  System.out.println("\n Spider ");
-   spider.eat();
-	 }
-}
+	 public class interFace { 
+		 public static void main(String [] args){
+				Scanner sc=new Scanner(System.in);
+				Fish fish=new Fish();
+				System.out.println("");
+				System.out.println("FISH");
+				fish.setName("Mimi");
+				System.out.println("This fish's name is "+fish.getName());
+				fish.eat();
+				fish.walk();
+				Fish fish2=new Fish();
+				fish2.setName("Momo");
+				System.out.println("This fish's name is "+fish2.getName());
+				
+				Cat cat=new Cat();
+				System.out.println("");
+				System.out.println("CAT");
+				cat.setName("Fluffy");
+				System.out.println("This cat's name is "+cat.getName());
+				cat.walk();
+				cat.eat();
+				cat.setName("Moose");
+				System.out.println("This cat's name is "+cat.getName());
 
-abstract class Animal{
-	protected int legs;
-	protected Animal(int legs) {
-		this.legs = legs;
-	}
+			    Spider spider=new Spider();
+			    System.out.println("");
+				System.out.println("SPIDER");
+				spider.eat();
+				spider.walk();
+				} 
+			}
+			abstract class Animal{
+				 protected int legs;
+				 protected Animal (int legs) {
+					 this.legs=legs;
+					  
+				 }
+				 public abstract void eat();
+				 public void walk() {
+					 System.out.println("This Animal walks on "+legs+ " legs");
+				 }
+			}
+			class Spider extends Animal{	 
+				public Spider(){
+					super(8);//All Spiders have eight legs
+				}
+				@Override
+				public void eat() {
+					System.out.println("Spider eat insects");
+				}
+			}
+			interface Pet{
+				 String getName();
+				 void setName (String name);
+				 void play();
+			}
+			//CAT
+			class Cat extends Animal implements Pet{
+				private String name;
+				public Cat(String name) {
+					super(4);// All cats have 4 legs
+					this.name=name;
+				}
+				Cat() {
+					 this("");
+				}
+				@Override
+				public void eat() {
+					System.out.println("Cat eat fishes");
+				}
+				@Override
+				public void setName(String name) {
+					this.name=name;
+					
+				}
+				@Override
+				public String getName() {
+					return this.name;
+					
+				}
+				public void play() {
+					System.out.println("The cat is playing");
+					
+				}
+			}
+			//FISH
+			class Fish extends Animal implements Pet{
+				private String name;
+				public Fish() {
+					super(0);// All fish have 0 legs
+					
+				}
+				
+				@Override
+				public void setName(String name) {
+					this.name=name;
+				}
+				public void play() {
+					System.out.println("The fish is playing");
+					
+				}
+				@Override
+				public String getName() {
+					return this.name;
+				}
+				@Override
+				public void walk() {
+					System.out.println("Fish has no legs");
+				}
+				@Override
+				public void eat() {
+					System.out.println("Fish eat plants");
+				}
+			}
 
-public abstract void eat ();
-public void walk() {
-	System.out.println("This is animal walks on "+legs+"legs");
-}
-}
-
-class Spider extends Animal{
-	public Spider() {
-		super(8);
-	}
-	public void eat() {
-		System.out.println("The Spider is eating");
-	}
-}
-
-interface Pet{
-	String getName();
-	void setName(String name);
-	void play();
-}
-
-//cat
-
-class Cat extends Animal implements Pet{
-	private String name;
-    public Cat(String name) {
-	super(4); //All cats have 4 legs
-	this . name=name;
-	}
-  Cat(){
-	this(" ");
-}
-
-@Override
-public void eat() {
-	System.out.println("The cat is eating");
-}
-
-@Override
-public void setName(String name) {
-	this.name = name;
-}
-
-@Override
-public String getName() {
-	return this.name;
-}
-public void play() {
-	System.out.println("The cat is playing");
-}
-}
-
- //fish 
-
-class Fish extends Animal implements Pet{
-	private String name;
-    public Fish() {
-	super(0); //All fish have 0 legs
-	}
-
-
-@Override
-public void eat() {
-	System.out.println("The fish eat plants ");
-}
- 
-public void setName(String name) {
-	this.name = name;
-}
-
-@Override
-public String getName() {
-	return this.name;
-}
-public void play() {
-	System.out.println("The Fish is playing");
-}
-
-@Override
-public void walk() {
-	System.out.println("The Fish has no legs ");
-}
-}
+			 
